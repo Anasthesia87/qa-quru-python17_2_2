818c2695e887f156e8d0e085f7ae69985cfa4517
Anastasiya_Lavretskaya <a.lavretskaya87@yandex.ru>
2024-11-15 20:53:20 +0300

    add new request and browser version

--- test_browser_search.py
+++ test_browser_search.py
@@ -2,12 +2,12 @@ from selene import browser, be, have
 
 
 def test_search_correct_request():
-    browser.open('https://google.com')
+    browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
 
 def test_search_incorrect_request():
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('y566/76554j===').press_enter()
-    browser.element('#rcnt').should(have.text('По запросу y566/76554j=== ничего не найдено. '))
+    browser.open('https://google.com/ncr')
+    browser.element('[name="q"]').should(be.blank).type('y566ЗQW/76554jASdнрг09=!!!!!!ююю!==йй').press_enter()
+    browser.element('#rcnt').should(have.text('Your search - y566ЗQW/76554jASdнрг09=!!!!!!ююю!==йй - did not match any documents.'))
